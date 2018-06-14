2b166ab2b04a61ef63ac29bd52c7db2e6aa2516a
hanchen.wang <hanchen.wang@gmail.com>
2018-06-13 23:39:13 -0400

    skeleton code for sqlite components

--- wealthsimple/source/app/routes.py
+++ wealthsimple/source/app/routes.py
@@ -1,7 +1,33 @@
 from flask import render_template
+from flask import g
+
 from app import app
+
+import sqlite3
+
+DATABASE = 'database.db'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+def init_db():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
 
 @app.route('/')
 def index():
     temp_user = {'username': 'Micro'}
     return render_template('index.html', user=temp_user)
+
