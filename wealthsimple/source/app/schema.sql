50da7abd45709a574e7c2937db31f61a3d50e708
Arash <anourimand@gmail.com>
2018-06-17 15:30:41 -0400

    added 6 mock users

--- wealthsimple/source/app/schema.sql
+++ wealthsimple/source/app/schema.sql
@@ -23,3 +23,39 @@ CREATE TABLE transactions (
   ToUserDisplay TEXT,
   Amount REAL
 );
+
+INSERT INTO users (
+  UserName,
+  Password,
+  DisplayName
+)
+VALUES
+("hackthesix+user17@wealthsimple.com", "hackthesix!","Big-Daddy Charlie"),
+("hackthesix+user+1@wealthsimple.com", "hackthesix!","Tony Stark"),
+("hackthesix+user+2@wealthsimple.com", "hackthesix!","Hanchen Wang"),
+("hackthesix+user+3@wealthsimple.com", "hackthesix!","Arash Nourimand"),
+("hackthesix+user+5@wealthsimple.com", "hackthesix!","Light Yagami"),
+("hackthesix+user+4@wealthsimple.com", "hackthesix!","Obi-Wan Kenobi"));
+
+-- INSERT INTO teams (
+--   TeamName,
+--   UserName,
+--   Password,
+--   Admin,
+--   Members
+-- )
+-- VALUES
+-- (
+--     "Team Jacob",
+--     "Team Edward"),
+-- (   "hackthesix+user17@wealthsimple.com",
+--     "hackthesix+user+1@wealthsimple.com",
+--     "hackthesix+user+10@wealthsimple.com"),
+-- (   "hackthesix!",
+--     "hackthesix!",
+--     "hackthesix!"),
+-- (   "Admin1",
+--     "Admin2"),
+-- (   "Big-Daddy Charlie",
+--     "Tony Stark",
+--     "Obi-Wan Kenobi");
