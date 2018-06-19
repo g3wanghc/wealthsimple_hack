f65578fbad9e3c891055139ae76e484a7a4ce8ce
hanchen.wang <hanchen.wang@gmail.com>
2018-06-18 21:38:53 -0700

    password grant acquire tokens on startup

--- wealthsimple/source/app/routes.py
+++ wealthsimple/source/app/routes.py
@@ -3,17 +3,25 @@ from flask import g
 
 from app import app
 
+import json
+import requests
 import sqlite3
 import yaml
 
 DATABASE = 'database.db'
 
-USER_TOKENS = []
-TEAM_TOKENS = []
+USER_TOKENS = {}
+TEAM_TOKENS = {}
 
-with open('/secrets.yaml', 'r') as stream:
+# -------------------------------------------------------------------
+
+with open('secrets.yaml', 'r') as stream:
     try:
-        print(yaml.load(stream))
+        secrets = yaml.load(stream)
+        CLIENT_ID = secrets['client_id']
+        CLIENT_SECRET = secrets['client_secret']
+        TOK_EXT_URL = secrets['token_exchange_url']
+        print("credentials acquired")
     except yaml.YAMLError as exc:
         print(exc)
 
@@ -31,18 +39,51 @@ def close_connection(exception):
     if db is not None:
         db.close()
 
-# import app
-# app.routes.init_db()
-
 def init_db():
     with app.app_context():
         db = get_db()
         with app.open_resource('schema.sql', mode='r') as f:
             db.cursor().executescript(f.read())
         db.commit()
-
     print("database initialized")
 
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
+# initialize testing DB
+init_db()
+
+# -------------------------------------------------------------------
+
+def password_grant_tok_exch(username, password, client_id = CLIENT_ID, client_secret = CLIENT_SECRET, scope = 'read write'):
+
+    payload = {
+        'grant_type': 'password',
+        'username': username,
+        'password': password,
+        'client_id': CLIENT_ID,
+        'client_secret': CLIENT_SECRET,
+        'scope': scope
+    }
+
+    response = requests.request("POST", TOK_EXT_URL, data=payload)
+    json_data = json.loads(response.text)
+    access_token = json_data['access_token']
+
+    return access_token
+
+with app.app_context():
+    for username, password in query_db('select username, password from users'):
+        USER_TOKENS[username] = password_grant_tok_exch(username = username, password = password)
+    print('users access tokens acquired')
+
+    for username, password in query_db('select username, password from teams'):
+        TEAM_TOKENS[username] = password_grant_tok_exch(username = username, password = password)
+    print('teams access tokens acquired')
+    
 # -------------------------------------------------------------------
 
 @app.route('/')
--- wealthsimple/source/app/schema.sql
+++ wealthsimple/source/app/schema.sql
@@ -3,62 +3,66 @@ DROP TABLE if exists teams;
 DROP TABLE if exists transactions;
 
 CREATE TABLE users (
-  UserName TEXT,
-  Password TEXT,
-  DisplayName TEXT
+  username TEXT,
+  password TEXT,
+  display_name TEXT
 );
 
 CREATE TABLE teams (
-  TeamName TEXT,
-  UserName TEXT,
-  Password TEXT,
-  Admin TEXT,
-  Members TEXT
+  team_name TEXT,
+  username TEXT,
+  password TEXT,
+  admin TEXT,
+  members TEXT
 );
 
 CREATE TABLE transactions (
-  FromUser TEXT,
-  FromUserDisplayName TEXT,
-  ToUser TEXT,
-  ToUserDisplay TEXT,
-  Amount REAL,
-  Description TEXT,
-  Team TEXT
+  from_user TEXT,
+  from_user_display_name TEXT,
+  to_user TEXT,
+  to_user_display_name TEXT,
+  amount REAL,
+  description TEXT,
+  team TEXT
 );
 
+-- testing data 
+
 INSERT INTO users (
-  UserName,
-  Password,
-  DisplayName
+  username,
+  password,
+  display_name
 )
 VALUES
-("hackthesix+user+1@wealthsimple.com", "hackthesix!","Tony Stark"),
-("hackthesix+user+2@wealthsimple.com", "hackthesix!","Hanchen Wang"),
-("hackthesix+user+3@wealthsimple.com", "hackthesix!","Arash Nourimand"),
-("hackthesix+user+4@wealthsimple.com", "hackthesix!","Light Yagami"),
-("hackthesix+user+5@wealthsimple.com", "hackthesix!","Obi-Wan Kenobi"),
-("hackthesix+user+6@wealthsimple.com", "hackthesix!","Big-Daddy Charlie");
+  ("hackthesix+user+1@wealthsimple.com", "hackthesix!","Tony Stark"),
+  ("hackthesix+user+2@wealthsimple.com", "hackthesix!","Hanchen Wang"),
+  ("hackthesix+user+3@wealthsimple.com", "hackthesix!","Arash Nourimand"),
+  ("hackthesix+user+4@wealthsimple.com", "hackthesix!","Light Yagami"),
+  ("hackthesix+user+5@wealthsimple.com", "hackthesix!","Obi-Wan Kenobi"),
+  ("hackthesix+user+6@wealthsimple.com", "hackthesix!","Big-Daddy Charlie");
 
-INSERT INTO transactions (
-  FromUser,
-  FromUserDisplayName,
-  ToUser,
-  ToUserDisplay,
-  Amount,
-  Description,
-  Team
-)
-VALUES
-  ("hackthesix+user+3@wealthsimple.com", "Arash Nourimand", "hackthesix+user+2@wealthsimple.com", "Hanchen Wang", "5.00", "Transferring money for the coca-cola","Team Edward"), 
-  ("hackthesix+user+6@wealthsimple.com", "Big-Daddy Charlie", "hackthesix+user+4@wealthsimple.com", "Light Yagami", "500.00", "Writing my ex wife's name in the notebook","Team Edward");
 
-  INSERT INTO teams (
-  TeamName,
-  UserName,
-  Password,
-  Admin,
-  Members
+INSERT INTO teams (
+  team_name,
+  username,
+  password,
+  admin,
+  members
 )
 VALUES
   ("Team Jacob", "hackthesix+user+7@wealthsimple.com", "hackthesix!", "Hanchen Wang", '["Hanchen Wang", "Tony Stark", "Light Yagami"]'), 
   ("Team Edward", "hackthesix+user+8@wealthsimple.com", "hackthesix!", "Arash Nourimand", '["Arash Nourimand", "Obi-Wan Kenobi", "Big-Daddy Charlie"]');
+
+
+INSERT INTO transactions (
+  from_user,
+  from_user_display_name,
+  to_user,
+  to_user_display_name,
+  amount,
+  description,
+  team
+)
+VALUES
+  ("hackthesix+user+3@wealthsimple.com", "Arash Nourimand", "hackthesix+user+2@wealthsimple.com", "Hanchen Wang", "5.00", "Transferring money for the coca-cola","Team Edward"), 
+  ("hackthesix+user+6@wealthsimple.com", "Big-Daddy Charlie", "hackthesix+user+4@wealthsimple.com", "Light Yagami", "500.00", "Writing my ex wife's name in the notebook","Team Edward");
