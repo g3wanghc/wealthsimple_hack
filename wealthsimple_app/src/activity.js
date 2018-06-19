895e721cf2ad1884a4bb263501c9ab46b1cb5146
hanchen.wang <hanchen.wang@gmail.com>
2018-06-19 15:04:59 -0700

    linting and add more todo for the backend app

--- wealthsimple_app/src/activity.js
+++ wealthsimple_app/src/activity.js
@@ -20,111 +20,111 @@ class Activity extends React.Component {
       <div>
       	<CustomNavbar loginHandler={this.props.loginHandler} pageHandler={this.props.pageHandler} />
       	<div className="container">
-					<h3>Activity</h3>
-					<Well bsSize="large">
-						<div className="activity-top">
-							<div className="item-space">
-								<DropdownButton
-						      title="Date"
-						      id="activity-date-dropdown"
-						    >
-						      <MenuItem eventKey="1">All time</MenuItem>
-						      <MenuItem eventKey="2">Past month</MenuItem>
-						      <MenuItem eventKey="3">Past year</MenuItem>
-						    </DropdownButton>
-					    </div>
-					    <div className="item-space">
-						    <ButtonToolbar>
-							    <ToggleButtonGroup type="checkbox">
-							      <ToggleButton value={1}>All accounts</ToggleButton>
-							      <ToggleButton value={2}>TFSA</ToggleButton>
-							    </ToggleButtonGroup>
-							  </ButtonToolbar>
-						  </div>
-						  <div className="item-space">
-							  <ButtonToolbar>
-							    <ToggleButtonGroup type="checkbox" defaultValue={[1]}>
-							      <ToggleButton value={1}>All activities</ToggleButton>
-							      <ToggleButton value={2}>Deposits</ToggleButton>
-							      <ToggleButton value={3}>Investments</ToggleButton>
-							      <ToggleButton value={4}>Sales</ToggleButton>
-							      <ToggleButton value={5}>Dividends and interest</ToggleButton>
-							      <ToggleButton value={6}>Fees and reimbursements</ToggleButton>
-							      <ToggleButton value={7}>Withdrawals</ToggleButton>
-							      <ToggleButton value={8}>Corrections</ToggleButton>
-							    </ToggleButtonGroup>
-							  </ButtonToolbar>
-							</div>
-						</div>
-					</Well>
-
-					<Well bsSize="large">
-						<div className="activity-bottom">
-							<div className="activity-items">
-								<Panel id="collapsible-panel-example-2">
-				          <Panel.Heading>
-				            <Panel.Title toggle>
-				              May 30 - Funds Invested (RRSP), Assets Sold (TFSA), Funds Invested (TFSA)
-				            </Panel.Title>
-				          </Panel.Heading>
-				          <Panel.Collapse>
-				            <Panel.Body>
-				            	<strong className="activity-items-left">Funds Invested (RRSP)</strong>
-				            	<strong className="activity-items-right">$473.43</strong>
-				              <p className="activity-items-left">Bought VAB (10 shares at $25.06/share)</p>
-				              <p className="activity-items-right">$250.60</p>
-				              <p className="activity-items-left">Bought XUS (4 shares at $43.91/share)</p>
-				              <p className="activity-items-right">$175.64</p>
-				              <p className="activity-items-left">Bought ZEF (4 shares at $15.73/share)</p>
-				              <p className="activity-items-right">$47.19</p>
-				              <strong className="activity-items-left">Assets Sold (TFSA)</strong>
-				              <strong className="activity-items-right">$101.60</strong>
-				              <p className="activity-items-left">Sold VIG ( shares at $101.60 USD/share)</p>
-				              <p className="activity-items-right">$101.60</p>
-				              <strong className="activity-items-left">Funds Invested (TFSA)</strong>
-				              <strong className="activity-items-right">$4241.28</strong>
-				              <p className="activity-items-left">Bought GOOG (4 shares at $1060.32 USD/share)</p>
-				              <p className="activity-items-right">$4241.28</p>
-				            </Panel.Body>
-				          </Panel.Collapse>
-				        </Panel>
-				        <Panel id="collapsible-panel-example-2">
-				          <Panel.Heading>
-				            <Panel.Title toggle>
-				              May 29 - Funds Invested (TFSA), Funds Invested (TFSA), Assets Sold (TFSA)
-				            </Panel.Title>
-				          </Panel.Heading>
-				          <Panel.Collapse>
-				            <Panel.Body>
-				            	<strong className="activity-items-left">Funds Invested (TFSA)</strong>
-				            	<strong className="activity-items-right">$10,355.68 USD</strong>
-				              <p className="activity-items-left">Bought GOOG (9 shares at $1060.32 USD/share)</p>
-				              <p className="activity-items-right">$9,542.88 USD</p>
-				              <p className="activity-items-left">Bought VIG (6 shares at $101.60 USD/share)</p>
-				              <p className="activity-items-right">$175.64</p>
-				              <p className="activity-items-left">Bought ZEF (4 shares at $15.73/share)</p>
-				              <p className="activity-items-right">$47.19</p>
-				              <strong className="activity-items-left">Funds Invested (RRSP)</strong>
-				              <strong className="activity-items-right">$600.56</strong>
-				              <p className="activity-items-left">Bought XUS (6 shares at $43.91/share)</p>
-				              <p className="activity-items-right">$263.46</p>
-				              <p className="activity-items-left">Bought BAM.A (5 shares at $51.69/share)</p>
-				              <p className="activity-items-right">$258.45</p>
-				              <p className="activity-items-left">Bought ZEF (5 shares at $15.73/share)</p>
-				              <p className="activity-items-right">$78.65</p>
-				              <strong className="activity-items-left">Assets Sold (TFSA)</strong>
-				              <strong className="activity-items-right">$800.93</strong>
-				              <p className="activity-items-left">Sold XUS (10 shares at $43.91/share)</p>
-				              <p className="activity-items-right">$439.10</p>
-				              <p className="activity-items-left">Sold BAM.A (7 shares at $51.69/share)</p>
-				              <p className="activity-items-right">$361.83</p>
-				            </Panel.Body>
-				          </Panel.Collapse>
-				        </Panel>
-			        </div>
-						</div>
-					</Well>
+			<h3>Activity</h3>
+			<Well bsSize="large">
+				<div className="activity-top">
+					<div className="item-space">
+						<DropdownButton
+				      title="Date"
+				      id="activity-date-dropdown"
+				    >
+				      <MenuItem eventKey="1">All time</MenuItem>
+				      <MenuItem eventKey="2">Past month</MenuItem>
+				      <MenuItem eventKey="3">Past year</MenuItem>
+				    </DropdownButton>
+			    </div>
+			    <div className="item-space">
+				    <ButtonToolbar>
+					    <ToggleButtonGroup type="checkbox">
+					      <ToggleButton value={1}>All accounts</ToggleButton>
+					      <ToggleButton value={2}>TFSA</ToggleButton>
+					    </ToggleButtonGroup>
+					  </ButtonToolbar>
+				  </div>
+				  <div className="item-space">
+					  <ButtonToolbar>
+					    <ToggleButtonGroup type="checkbox" defaultValue={[1]}>
+					      <ToggleButton value={1}>All activities</ToggleButton>
+					      <ToggleButton value={2}>Deposits</ToggleButton>
+					      <ToggleButton value={3}>Investments</ToggleButton>
+					      <ToggleButton value={4}>Sales</ToggleButton>
+					      <ToggleButton value={5}>Dividends and interest</ToggleButton>
+					      <ToggleButton value={6}>Fees and reimbursements</ToggleButton>
+					      <ToggleButton value={7}>Withdrawals</ToggleButton>
+					      <ToggleButton value={8}>Corrections</ToggleButton>
+					    </ToggleButtonGroup>
+					  </ButtonToolbar>
+					</div>
 				</div>
+			</Well>
+
+			<Well bsSize="large">
+				<div className="activity-bottom">
+					<div className="activity-items">
+						<Panel id="collapsible-panel-example-2">
+		          <Panel.Heading>
+		            <Panel.Title toggle>
+		              May 30 - Funds Invested (RRSP), Assets Sold (TFSA), Funds Invested (TFSA)
+		            </Panel.Title>
+		          </Panel.Heading>
+		          <Panel.Collapse>
+		            <Panel.Body>
+		            	<strong className="activity-items-left">Funds Invested (RRSP)</strong>
+		            	<strong className="activity-items-right">$473.43</strong>
+		              <p className="activity-items-left">Bought VAB (10 shares at $25.06/share)</p>
+		              <p className="activity-items-right">$250.60</p>
+		              <p className="activity-items-left">Bought XUS (4 shares at $43.91/share)</p>
+		              <p className="activity-items-right">$175.64</p>
+		              <p className="activity-items-left">Bought ZEF (4 shares at $15.73/share)</p>
+		              <p className="activity-items-right">$47.19</p>
+		              <strong className="activity-items-left">Assets Sold (TFSA)</strong>
+		              <strong className="activity-items-right">$101.60</strong>
+		              <p className="activity-items-left">Sold VIG ( shares at $101.60 USD/share)</p>
+		              <p className="activity-items-right">$101.60</p>
+		              <strong className="activity-items-left">Funds Invested (TFSA)</strong>
+		              <strong className="activity-items-right">$4241.28</strong>
+		              <p className="activity-items-left">Bought GOOG (4 shares at $1060.32 USD/share)</p>
+		              <p className="activity-items-right">$4241.28</p>
+		            </Panel.Body>
+		          </Panel.Collapse>
+		        </Panel>
+		        <Panel id="collapsible-panel-example-2">
+		          <Panel.Heading>
+		            <Panel.Title toggle>
+		              May 29 - Funds Invested (TFSA), Funds Invested (TFSA), Assets Sold (TFSA)
+		            </Panel.Title>
+		          </Panel.Heading>
+		          <Panel.Collapse>
+		            <Panel.Body>
+		            	<strong className="activity-items-left">Funds Invested (TFSA)</strong>
+		            	<strong className="activity-items-right">$10,355.68 USD</strong>
+		              <p className="activity-items-left">Bought GOOG (9 shares at $1060.32 USD/share)</p>
+		              <p className="activity-items-right">$9,542.88 USD</p>
+		              <p className="activity-items-left">Bought VIG (6 shares at $101.60 USD/share)</p>
+		              <p className="activity-items-right">$175.64</p>
+		              <p className="activity-items-left">Bought ZEF (4 shares at $15.73/share)</p>
+		              <p className="activity-items-right">$47.19</p>
+		              <strong className="activity-items-left">Funds Invested (RRSP)</strong>
+		              <strong className="activity-items-right">$600.56</strong>
+		              <p className="activity-items-left">Bought XUS (6 shares at $43.91/share)</p>
+		              <p className="activity-items-right">$263.46</p>
+		              <p className="activity-items-left">Bought BAM.A (5 shares at $51.69/share)</p>
+		              <p className="activity-items-right">$258.45</p>
+		              <p className="activity-items-left">Bought ZEF (5 shares at $15.73/share)</p>
+		              <p className="activity-items-right">$78.65</p>
+		              <strong className="activity-items-left">Assets Sold (TFSA)</strong>
+		              <strong className="activity-items-right">$800.93</strong>
+		              <p className="activity-items-left">Sold XUS (10 shares at $43.91/share)</p>
+		              <p className="activity-items-right">$439.10</p>
+		              <p className="activity-items-left">Sold BAM.A (7 shares at $51.69/share)</p>
+		              <p className="activity-items-right">$361.83</p>
+		            </Panel.Body>
+		          </Panel.Collapse>
+		        </Panel>
+	        </div>
+			</div>
+			</Well>
+		</div>
   		</div>
     );
   }
